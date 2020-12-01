962f09d92a2864d3f4363c9040c0c22f5dbe83c5
Natalia Meneses <nmeneses8@gmail.com>
2020-12-01 17:10:12 -0500

    Primer commit socket server

--- models/server.js
+++ models/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
+
 const Sockets = require('./sockets');
 
 class Server {
@@ -20,6 +22,9 @@ class Server {
     middlewares() {
         //Desplegar el directorio público
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+        //CORS
+        this.app.use(cors());
     }
 
     configurarSockets() {
